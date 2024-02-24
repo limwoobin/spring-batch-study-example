6e097fe154b342444a9b1be00b2132efa5b7aad6
limwoobin <drogba02@naver.com>
2024-02-24 14:55:52 +0900

    feat: ClassifierItemProcessor step modify

--- src/main/java/com/example/batchexample/batch/classifier/ClassifierSimpleStepConfig.java
+++ src/main/java/com/example/batchexample/batch/classifier/ClassifierSimpleStepConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Configuration
